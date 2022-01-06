579e3f2d02af9eefa0927be75b54f3c57d899e43
Zach Kanzler <they4kman@gmail.com>
2022-01-06 00:04:30 -0500

    chore: remove old Go stuff

--- btrfs/constants.go
+++
@@ -1,7 +0,0 @@
-package btrfs
-
-const BTRFS_MAGIC = "_BHRfS_M"
-const BTRFS_UUID_SIZE = 16
-const BTRFS_LABEL_SIZE = 256
-const BTRFS_CSUM_SIZE = 32
-const BTRFS_FSID_SIZE = 16
--- go.mod
+++
@@ -1,5 +0,0 @@
-module btrfs-recon
-
-go 1.17
-
-require github.com/google/uuid v1.3.0
