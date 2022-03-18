21eba6493e61d080119bb246cf91c56b98cbf8c7
Zach Kanzler <they4kman@gmail.com>
2022-03-18 03:35:35 -0400

    fix(schema): resolve FileExtentItem type=REGULAR fields not being filled out

--- btrfs_recon/persistence/serializers/file_extent_item.py
+++ btrfs_recon/persistence/serializers/file_extent_item.py
@@ -1,3 +1,5 @@
+import marshmallow as ma
+
 from btrfs_recon import structure
 from btrfs_recon.persistence import models
 from .base import LeafItemDataSchema
@@ -12,3 +14,13 @@ class FileExtentItemSchema(LeafItemDataSchema):
         model = models.FileExtentItem
         struct_class = structure.FileExtentItem
         key_type = structure.KeyType.ExtentData
+        version = 1
+
+    @ma.pre_load
+    def before_load(self, data, **kwargs):
+        # Because marshmallow, for whatever reason, doesn't support deep data_keys
+        # (i.e. dotted paths), we raise our ref.* keys to toplevel.
+        if data['ref']:
+            data.update(data['ref'])
+
+        return data
